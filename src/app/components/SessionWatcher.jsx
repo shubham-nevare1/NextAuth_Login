de558d3b6192296889e4004f922124d84f42105e
shubham-nevare1 <shubham.nevare@schbang.com>
2025-05-13 15:16:29 +0530

    remove unused code

--- src/app/components/SessionWatcher.jsx
+++ src/app/components/SessionWatcher.jsx
@@ -1,41 +1,10 @@
-// "use client";
-
-// import { useSession, signOut } from "next-auth/react";
-// import { useEffect } from "react";
-
-// export default function SessionWatcher() {
-//   const { data: session } = useSession();
-
-//   useEffect(() => {
-//     if (!session?.expires) return;
-
-//     const interval = setInterval(() => {
-//       const now = new Date();
-//       const expiry = new Date(session.expires);
-
-//       if (now >= expiry) {
-//         clearInterval(interval);
-//         const confirmLogout = confirm("Session expired. Please log in again.");
-//         if (confirmLogout) {
-//           signOut({ callbackUrl: "/" });
-//         }
-//       }
-//     }, 5000);
-
-//     return () => clearInterval(interval);
-//   }, [session]);
-
-//   return null;
-// }
 "use client";
 
 import { useSession, signOut } from "next-auth/react";
 import { useEffect } from "react";
-import { usePathname } from "next/navigation"; // <-- App Router
 
 export default function SessionWatcher() {
   const { data: session } = useSession();
-  const pathname = usePathname();
 
   useEffect(() => {
     if (!session?.expires) return;
@@ -51,15 +20,16 @@ export default function SessionWatcher() {
       //     signOut({ callbackUrl: "/" });
       //   }
       // }
-      if (now >= expiry) {
-        clearInterval(interval);
-        alert("🔒 Session expired. You will be logged out.");
-        signOut({ callbackUrl: "/" });
-      }
+        if (now >= expiry) {
+          clearInterval(interval);
+          alert("🔒 Session expired. You will be logged out.");
+          signOut({ callbackUrl: "/" });
+        }
     }, 5000);
 
     return () => clearInterval(interval);
-  }, [session?.expires, pathname]); // re-run on route change
+  }, [session]);
 
   return null;
 }
+
