2725b71fd171abe65c041324d9c0efd0d127960d
shubham-nevare1 <shubham.nevare@schbang.com>
2025-05-23 12:23:34 +0530

    add useEffect

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,6 +2,7 @@
 
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function ContactPage() {
     const { data: session, status } = useSession();
